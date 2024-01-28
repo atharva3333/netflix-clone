d0905c6b796e7294806adf0e81bbf2371a1ec217
Atharva <scifacts001@gmail.com>
2024-01-28 10:54:33 +0530

    Kids section client state

--- src/components/Genre/KidsMovies.jsx
+++ src/components/Genre/KidsMovies.jsx
@@ -1,30 +1,12 @@
-import { useState, useEffect } from "react";
-import { txtDB } from "../../firebase.config";
-import { collection, getDocs } from "firebase/firestore";
-import ScrollContents from "../common/ScrollContents";
+import KidsMoviesData from "../../data/kidsmovies.json"
+import ScrollContents from "../common/ScrollContents"
 
 const KidsMovies = () => {
-  const [kidMovies, setKidMovies] = useState([]);
-  
-  
-  const getData = async () => {
-    const valRef = collection(txtDB, "txtData");
-    const dataDb = await getDocs(valRef);
-    const allData = dataDb.docs.map((val) => ({ ...val.data(), id: val.id }));
-    setKidMovies(allData);
-    console.log(dataDb);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  
-
+ 
   return (
-    <>
-  <ScrollContents title={"Kids"} data={kidMovies}/>
-    </>
+    <div>
+  <ScrollContents title={"Kids"} data={KidsMoviesData}/>
+    </div>
   );
 };
 
